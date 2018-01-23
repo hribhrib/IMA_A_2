bb9c8c770ff4ef1fdf5941688b6b522d9935f68a
hribhrib <chribar@gmx.at>
2018-01-23 18:15:27 +0100

    animation (raw)

---
+++ app/src/main/java/hribhrib/ima_a_2/AnimationActivity.java
@@ -0,0 +1,92 @@
+package hribhrib.ima_a_2;
+
+import android.animation.ValueAnimator;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class AnimationActivity extends AppCompatActivity {
+
+    ArrayList<TextView> wordList = new ArrayList<TextView>();
+    TextView tv;
+    ConstraintLayout ll;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation);
+
+
+        String myString;
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                myString= null;
+            } else {
+                myString= extras.getString("String");
+            }
+        } else {
+            myString= (String) savedInstanceState.getSerializable("String");
+        }
+
+
+
+//       tv = new TextView(this);
+  //      tv.setTextSize(20);
+    //    tv.setText(myString);
+
+        ll = findViewById(R.id.animation);
+
+
+        splitString(myString);
+
+        for (TextView t : wordList){
+            ll.addView(t);
+        }
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        System.out.println(metrics.heightPixels);
+
+        ValueAnimator animation = ValueAnimator.ofFloat(0f, 500f);
+        animation.setDuration(1000);
+        animation.start();
+
+        animation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator updatedAnimation) {
+                // You can use the animated value in a property that uses the
+                // same type as the animation. In this case, you can use the
+                // float value in the translationX property.
+                float animatedValue = (float)updatedAnimation.getAnimatedValue();
+
+                float tmp = 0;
+
+                for(TextView t : wordList){
+                    t.setTranslationY(animatedValue-tmp);
+                    tmp += t.getHeight()+10;
+
+                }
+
+                System.out.println(animatedValue);
+            }
+        });
+
+    }
+
+    private void splitString(String str){
+        String[] ar = str.split("\\s+");
+        for (String s : ar){
+            TextView tmp = new TextView(ll.getContext());
+            tmp.setTextSize(20);
+            tmp.setText(s);
+            wordList.add(tmp);
+        }
+    }
+}
--- app/src/main/java/hribhrib/ima_a_2/MainActivity.java
+++ app/src/main/java/hribhrib/ima_a_2/MainActivity.java
@@ -1,28 +1,56 @@
 package hribhrib.ima_a_2;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
+
+import java.security.Key;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText input;
     LinearLayout list;
 
+    AlertDialog.Builder myBuilder;
+    AlertDialog alertEmpty;
+
+    TextView tv;
+
+    int tvID = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myBuilder = new AlertDialog.Builder(this);
+
+        myBuilder.setTitle("Input Error");
+        myBuilder.setMessage("Empty String is not allowed!");
+        myBuilder.setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+
+        alertEmpty = myBuilder.create();
+
+
         input = (EditText) findViewById(R.id.inputText);
 
         input.setOnEditorActionListener(enterListener);
 
         list = (LinearLayout) findViewById(R.id.list);
+
 
     }
 
@@ -31,14 +59,40 @@ public class MainActivity extends AppCompatActivity {
         public boolean onEditorAction(TextView inputView, int actionId, KeyEvent event) {
             if (actionId == EditorInfo.IME_ACTION_DONE
                     ) {
-                System.out.println("test");
-                TextView tv = new TextView(list.getContext());
-                tv.setText(inputView.getText());
+                if (inputView.getText().toString().trim().equals("")) {
+                    alertEmpty.show();
+                } else {
+                    tv = new TextView(list.getContext());
+                    tv.setText(inputView.getText());
 
+                    tv.setHeight(60);
+                    tv.setTextSize(20);
+                    tv.setId(tvID++);
 
-                list.addView(tv);
+                    tv.setOnClickListener(touchListener);
+
+                    list.addView(tv);
+                }
             }
             return true;
         }
     };
+
+
+
+    TextView.OnClickListener touchListener = new TextView.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            TextView tmp = findViewById(v.getId());
+            launchAnimationActivity(tmp.getText().toString());
+        }
+
+    };
+
+    private void launchAnimationActivity(String str) {
+
+        Intent intent = new Intent(this, AnimationActivity.class);
+        intent.putExtra("String", str);
+        startActivity(intent);
+    }
 }
